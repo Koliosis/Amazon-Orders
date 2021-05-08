e2dc389facb2c0cd30c50d93940fe9e00afe361d
Kole <kstevendavis@gmail.com>
2021-05-07 18:19:48 -0600

    trying not to add all of python

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("does this work?")
+import csv
+
+with open('amazon-orders.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for range(10) in reader:
+        print(row['Order Date'])
