1d86783d5f148f412689503d281c33a9170ac8da
Kole <kstevendavis@gmail.com>
2021-05-13 19:40:01 -0600

    Made it so the program totals the amount spent

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ import csv
 
 with open('amazon-orders.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
-    for range(10) in reader:
-        print(row['Order Date'])
+    sum = 0
+    str_list = []
+    int_list = []
+    for row in reader:
+        str_list.append(row['Total Charged'])
+    my_ints = [float(l[1:]) for l in str_list]
+    print(my_ints)
+    total = 0
+    for i in my_ints:
+        total += i
+    print(total)
+        # sum += row['Total Charged']
